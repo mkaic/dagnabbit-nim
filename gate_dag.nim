3f6e26c57f4968918011943e77a0f6b1535e9ac5
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-06 16:40:38 -0700

    i have no idea what i'm doing

--- gate_dag.nim
+++ gate_dag.nim
@@ -1,9 +1,9 @@
-# type 
-#   Gate = ref object
-#     id: int
-#     inputs: tuple[Gate, Gate]
-#     outputs: seq[Gate]
-#     value: int
+type 
+  Gate = ref object
+    id: int
+    # inputs: tuple[Gate, Gate]
+    # outputs: seq[Gate]
+    value: int
 
 # proc nand(g: Gate): bool
 #   g.value = not (g.inputs[0].value and g.inputs[1].value)
