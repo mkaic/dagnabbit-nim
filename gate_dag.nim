8e00baa60c031c56d8e732dbb14053acaa5aca72
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-06 16:33:45 -0700

    hello world

---
+++ gate_dag.nim
@@ -0,0 +1,12 @@
+# type 
+#   Gate = ref object
+#     id: int
+#     inputs: tuple[Gate, Gate]
+#     outputs: seq[Gate]
+#     value: int
+
+# proc nand(g: Gate): bool
+#   g.value = not (g.inputs[0].value and g.inputs[1].value)
+
+echo "Hello, World!"
+# compile with `nim c -r gate_dag.nim`
