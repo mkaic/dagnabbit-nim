b8cc7cc28adbf5eb79ed1fd16f8cf198f6d52cd1
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-07 01:16:35 -0700

    eepy

--- gate_dag.nim
+++ gate_dag.nim
@@ -1,10 +1,28 @@
-type Gate = ref object
-  id: int
-  inputs: array[2, Gate]
-  outputs: seq[Gate]
-  value: bool
-
-proc nand(g: Gate): bool =
-  g.value = not (g.inputs[0].value and g.inputs[1].value)
+# import std/sequtils
+# import std/sugar
 
 # compile with `nim c -r --hints:off gate_dag.nim`
+type
+  NodeKind = enum
+    nkInput,
+    nkOutput,
+    nkGate
+
+  Node = ref object
+    kind: NodeKind
+    id: int
+    value: bool
+    inputs: seq[Node]
+    outputs: seq[Node]
+
+  Graph = object
+    inputs: seq[Node]
+    outputs: seq[Node]
+    gates: seq[Node]
+
+proc nand(self: Node) =
+  assert self.kind == nkGate, "nand can only be called on Nodes"
+  self.value = not (self.inputs[0].value and self.inputs[1].value)
+
+proc add_gate(self: Graph) =
+  let possible_inputs = self.inputs & self.gates
