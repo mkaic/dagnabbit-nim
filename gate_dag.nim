cc614973f3936c7d7b588cb654cf26d73aebad03
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-06 17:00:30 -0700

    i think i'm getting the hang of it

--- gate_dag.nim
+++ gate_dag.nim
@@ -1,12 +1,10 @@
-type 
-  Gate = ref object
-    id: int
-    inputs: seq[Gate]
-    outputs: seq[Gate]
-    value: int
+type Gate = ref object
+  id: int
+  inputs: seq[Gate]
+  outputs: seq[Gate]
+  value: int
 
-# proc nand(g: Gate): bool
-#   g.value = not (g.inputs[0].value and g.inputs[1].value)
+proc nand(g: Gate): bool =
+  g.value = not (g.inputs[0].value and g.inputs[1].value)
 
-echo "Hello, World!"
 # compile with `nim c -r gate_dag.nim`
