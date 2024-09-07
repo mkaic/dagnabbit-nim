4363fa7ff859849969c2506761b670cc53e6e273
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-06 17:14:14 -0700

    i am beginning to see how this works i think

--- gate_dag.nim
+++ gate_dag.nim
@@ -1,11 +1,10 @@
 type Gate = ref object
   id: int
-  input_a: Gate
-  input_b: Gate
+  inputs: array[2, Gate]
   outputs: seq[Gate]
-  value: int
+  value: bool
 
 proc nand(g: Gate): bool =
-  g.value = not (g.input_a.value and g.input_b.value)
+  g.value = not (g.inputs[0].value and g.inputs[1].value)
 
 # compile with `nim c -r --hints:off gate_dag.nim`
