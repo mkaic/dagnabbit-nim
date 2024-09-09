72c150651d6231dfd10d05aa4304a6c3e15901f3
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-09 12:27:56 -0700

    this is harder than I expected it to be

--- gate_dag.nim
+++ gate_dag.nim
@@ -1,16 +1,16 @@
 # import std/sequtils
-# import std/sugar
+import std/sugar
 import std/tables
 import std/strformat
 
 # compile with `nim c -r --hints:off gate_dag.nim`
 type
   Input = ref object
-    id: string
+    id: int
     value: bool
 
   Gate = ref object
-    id: string
+    id: int
     value: bool
     evaluated: bool
     inputs: array[2, string]
@@ -18,14 +18,52 @@ type
 
   Graph = object
     global_counter: int = 0
-    inputs: Table[string, Input] = initTable[string, Input]()
-    outputs: Table[string, Gate] = initTable[string, Gate]()
-    gates: Table[string, Gate] = initTable[string, Gate]()
+    inputs: seq[Input]
+    outputs: seq[Gate]
+    gates: seq[Gate]
+
+proc evaluate_gate(graph: Graph, gate: Gate): bool =
+  if not gate.evaluated:
+    gate.value
+
 
 proc add_input(self: var Graph) =
   var node_id = &"i{self.global_counter}"
-  self.inputs[node_id] = Input(id: node_id, value: false)
+  self.inputs.add(Input(id: node_id, value: false))
   self.global_counter += 1
+
+proc add_gate(self: var Graph) =
+  var node_id = &"g{self.global_counter}"
+  self.gates.add(
+    Gate(
+      id: node_id,
+      value: false,
+      evaluated: false,
+      inputs: ["", ""],
+      outputs: @[],
+    )
+  )
+  self.global_counter += 1
+
+proc add_output(self: var Graph) =
+  var node_id = &"o{self.global_counter}"
+  self.outputs.add(
+    Gate(
+      id: node_id,
+      value: false,
+      evaluated: false,
+      inputs: ["", ""],
+      outputs: @[],
+    )
+  )
+  self.global_counter += 1
+
+proc evaluate(self: var Graph): seq[bool] =
+  for o in self.outputs.values():
+    o.evaluate_gate()
+
+  return collect(newSeq):
+    for o in self.outputs.values(): o.value
 
 var test_graph = Graph()
 
@@ -37,7 +75,7 @@ test_graph.gates["g0"] = Gate(
   value: false,
   evaluated: false,
   inputs: ["i0", "i1"],
-  outputs: @["o0"], 
+  outputs: @["o0"],
   )
 
 test_graph.outputs["o0"] = Gate(
