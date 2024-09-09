7a5eef84b57884db4a7660be6537c05f4f8294cc
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-09 15:02:14 -0700

    implemented get_descendants and overload

--- gate_dag.nim
+++ gate_dag.nim
@@ -1,87 +1,79 @@
 # import std/sequtils
 import std/sugar
-import std/tables
-import std/strformat
 
 # compile with `nim c -r --hints:off gate_dag.nim`
 type
-  Input = ref object
-    id: int
-    value: bool
-
   Gate = ref object
-    id: int
     value: bool
     evaluated: bool
-    inputs: array[2, string]
-    outputs: seq[string]
+    inputs: array[2, Gate]
+    outputs: seq[Gate]
 
   Graph = object
-    global_counter: int = 0
-    inputs: seq[Input]
-    outputs: seq[Gate]
+    inputs: seq[Gate]
     gates: seq[Gate]
+    outputs: seq[Gate]
 
-proc evaluate_gate(graph: Graph, gate: Gate): bool =
+proc evaluate_gate(gate: Gate): bool =
   if not gate.evaluated:
-    gate.value
+    gate.value = not (gate.inputs[0].evaluate_gate() and gate.inputs[
+        1].evaluate_gate())
+    gate.evaluated = true
 
+  return gate.value
 
-proc add_input(self: var Graph) =
-  var node_id = &"i{self.global_counter}"
-  self.inputs.add(Input(id: node_id, value: false))
-  self.global_counter += 1
-
-proc add_gate(self: var Graph) =
-  var node_id = &"g{self.global_counter}"
-  self.gates.add(
-    Gate(
-      id: node_id,
-      value: false,
-      evaluated: false,
-      inputs: ["", ""],
-      outputs: @[],
-    )
-  )
-  self.global_counter += 1
-
-proc add_output(self: var Graph) =
-  var node_id = &"o{self.global_counter}"
-  self.outputs.add(
-    Gate(
-      id: node_id,
-      value: false,
-      evaluated: false,
-      inputs: ["", ""],
-      outputs: @[],
-    )
-  )
-  self.global_counter += 1
-
-proc evaluate(self: var Graph): seq[bool] =
-  for o in self.outputs.values():
-    o.evaluate_gate()
-
+proc evaluate_graph(self: var Graph): seq[bool] =
   return collect(newSeq):
-    for o in self.outputs.values(): o.value
+    for o in self.outputs:
+      o.evaluate_gate()
+
+proc reset(graph: var Graph) =
+  for g in graph.gates:
+    g.evaluated = false
+  for g in graph.outputs:
+    g.evaluated = false
+
+proc add_input(graph: var Graph, value: bool) =
+  graph.inputs.add(Gate(value: value, evaluated: true))
+
+proc add_gate(graph: var Graph, inputs: array[2, Gate],
+    is_output: bool = false) =
+  var g = Gate(inputs: inputs)
+  for i in inputs:
+    i.outputs.add(g)
+
+  if is_output:
+    graph.outputs.add(g)
+  else:
+    graph.gates.add(g)
+
+proc get_descendants(gate: Gate, known: var seq[Gate]): seq[Gate] =
+  for o in gate.outputs:
+    if o notin known:
+      known.add(o)
+      discard get_descendants(o, known = known)
+  return known
+
+# This overloads the first get_descendants proc and tells it what to do if "known" isn't passed.
+proc get_descendants(gate: Gate): seq[Gate] =
+  var known = newSeq[Gate]()
+  return get_descendants(gate, known = known)
 
 var test_graph = Graph()
 
-test_graph.add_input()
-test_graph.add_input()
+const input_values = [true, false, false, true, false, false, true, true]
+for i in input_values:
+  test_graph.add_input(i)
 
-test_graph.gates["g0"] = Gate(
-  id: "g0",
-  value: false,
-  evaluated: false,
-  inputs: ["i0", "i1"],
-  outputs: @["o0"],
-  )
+test_graph.add_gate(inputs = [test_graph.inputs[0], test_graph.inputs[1]])
+test_graph.add_gate(inputs = [test_graph.inputs[1], test_graph.inputs[1]])
 
-test_graph.outputs["o0"] = Gate(
-  id: "o0",
-  value: false,
-  evaluated: false,
-  inputs: ["g0", "g0"],
-  outputs: @[],
-  )
+test_graph.add_gate(inputs = [test_graph.inputs[0], test_graph.gates[0]],
+    is_output = true)
+
+let outputs = test_graph.evaluate_graph()
+echo outputs
+
+let children = get_descendants(test_graph.inputs[0])
+for c in children:
+  echo c.value
