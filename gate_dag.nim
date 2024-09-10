d1d90be72ae469d762d26467935cb59868b3bd26
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-10 15:15:53 -0700

    this is working surprisingly well

--- gate_dag.nim
+++ gate_dag.nim
@@ -5,7 +5,6 @@ import std/bitops
 
 randomize()
 
-# compile with `nim c -r --hints:off gate_dag.nim`
 type
   Gate = ref object
     value: uint64
@@ -30,7 +29,7 @@ proc evaluate_gate*(gate: Gate): uint64 =
 
   return gate.value
 
-proc evaluate_graph*(self: var Graph): seq[bool] =
+proc evaluate_graph*(self: var Graph): seq[uint64] =
   return collect(newSeq):
     for o in self.outputs:
       o.evaluate_gate()
@@ -42,7 +41,7 @@ proc reset*(graph: var Graph) =
     g.evaluated = false
 
 proc add_input*(graph: var Graph) =
-  graph.inputs.add(Gate(value: true, evaluated: true))
+  graph.inputs.add(Gate(value: 0'u64, evaluated: true))
 
 
 # proc get_ancestors(gate: Gate, known: var seq[Gate]): seq[Gate] =
@@ -74,7 +73,7 @@ proc init_gate*(graph: var Graph, output: bool = false) =
   else:
     graph.gates.add(g)
 
-proc set_inputs*(graph: var Graph, input_values: seq[bool]) =
+proc set_inputs*(graph: var Graph, input_values: seq[uint64]) =
   for i, v in input_values:
     graph.inputs[i].value = v
 
@@ -88,3 +87,70 @@ proc bool_seq_to_int*(seq: seq[bool]): int =
     if v:
       output = output or (1 shl i)
   return output
+
+proc transpose_bitmatrix*(matrix: seq[seq[bool]]): seq[seq[bool]] =
+  let rowsize = matrix[0].len
+
+  return collect(newSeq):
+    for i in 0 ..< rowsize:
+      collect(newSeq):
+        for row in matrix:
+          row[i]
+
+proc make_bitpacked_uint64_batches*(height: int, width: int, channels: int): (seq[seq[uint64]], int) =
+
+  let
+    x_bitcount = fast_log_2(width) + 1
+    y_bitcount = fast_log_2(height) + 1
+    c_bitcount = fast_log_2(channels) + 1
+    pos_bitcount = x_bitcount + y_bitcount + c_bitcount
+
+    y_as_bits = collect(newSeq):
+      for y in 0 ..< height:
+        y.int_to_bool_seq(bits=y_bitcount)
+
+    x_as_bits = collect(newSeq):
+      for x in 0 ..< width:
+        x.int_to_bool_seq(bits=x_bitcount)
+
+    c_as_bits = collect(newSeq):
+      for c in 0 ..< channels:
+        c.int_to_bool_seq(bits=c_bitcount)
+
+    total_iterations = width * height * channels
+    batch_count = (total_iterations + 1) div 64
+
+  var batches: seq[seq[uint64]]
+  for batch_idx in 0 ..< batch_count:
+
+    var batch_of_pos_bits: seq[seq[bool]]
+    for i in 0 ..< 64:
+      let
+        idx = batch_idx * 64 + i
+        c = idx div (height * width) mod channels
+        y = idx div (width) mod height
+        x = idx div (1) mod width
+
+        x_bits = x_as_bits[x]
+        y_bits = y_as_bits[y]
+        c_bits = c_as_bits[c]
+
+      batch_of_pos_bits.add(x_bits & y_bits & c_bits)
+
+    let batch: seq[uint64] = collect(newSeq):
+      for bits in batch_of_pos_bits.transpose_bitmatrix():
+        cast[uint64](bits.bool_seq_to_int()) 
+
+    batches.add(batch)
+
+  return (batches, pos_bitcount)
+
+proc init_as_reconstructor*(graph: var Graph, input_size: int, output_size: int, num_gates: int) =
+  for i in 0 ..< input_size:
+    graph.add_input()
+
+  for i in 0 ..< num_gates:
+    graph.init_gate()
+
+  for i in 0 ..< output_size:
+    graph.init_gate(output = true)
--- test.nim
+++ test.nim
@@ -1,57 +1,41 @@
 import ./gate_dag
 import pixie as pix
 # import std/strformat
-import std/bitops
 import std/sugar
-
-var test_graph = Graph()
 
 var branos = pix.read_image("branos.png")
 
 const 
-  w = 64
-  h = 64
+  width = 64
+  height = 64
+  channels = 3
 
-let
-  x_bitcount = fast_log_2(w) + 1
-  y_bitcount = fast_log_2(h) + 1
+branos = branos.resize(width, height)
+var result_image = pix.new_image(width, height)
 
-branos = branos.resize(w, h)
-var result_image = pix.new_image(w, h)
+let (batches, bitcount) = make_bitpacked_uint64_batches(height=height, width=width, channels=channels)
 
-var y_as_bits = collect(newSeq):
-  for y in 0 ..< h:
-    y.int_to_bool_seq(bits=y_bitcount)
+echo bitcount
+var graph = Graph()
+graph.init_as_reconstructor(input_size=bitcount, output_size=8, num_gates=32)
 
-var x_as_bits = collect(newSeq):
-  for x in 0 ..< w:
-    x.int_to_bool_seq(bits=x_bitcount)
+var outputs: seq[seq[uint64]]
+for batch in batches:
+  graph.set_inputs(batch)
+  outputs.add(graph.evaluate_graph())
+  graph.reset()
 
-var c_as_bits = collect(newSeq):
-  for c in 0 ..< 3:
-    c.int_to_bool_seq(bits=2)
+# let readable_outputs = unpack_uint64_outputs_to_uint8_hwc(outputs)
 
-for i in 1..(x_bitcount + y_bitcount + 3):
-  test_graph.add_input()
 
-for i in 1..20:
-  test_graph.init_gate()
+#       rgb[c] = cast[uint8](output.bool_seq_to_int())
 
-for i in 1..8:
-  test_graph.init_gate(output = true)
+#     result_image.unsafe[x, y] = pix.rgba(rgb[0], rgb[1], rgb[2], 255)
 
-      test_graph.set_inputs(pos_bits)
-      let output = test_graph.evaluate_graph()
-      test_graph.reset()
+#     let
+#       rgbx = branos.unsafe[x, y]
+#       r = rgbx.r # these are uint8
+#       g = rgbx.g
+#       b = rgbx.b
 
-      rgb[c] = cast[uint8](output.bool_seq_to_int())
-
-    result_image.unsafe[x, y] = pix.rgba(rgb[0], rgb[1], rgb[2], 255)
-
-    let
-      rgbx = branos.unsafe[x, y]
-      r = rgbx.r # these are uint8
-      g = rgbx.g
-      b = rgbx.b
-
-result_image.write_file("output_image.png")
+# result_image.write_file("output_image.png")
