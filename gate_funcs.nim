86069b96e48c154592ed2c5a7b0a4ceee01e568f
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-13 12:21:22 -0700

    different sets of logic gates don't have a huge effect

--- gate_funcs.nim
+++ gate_funcs.nim
@@ -6,18 +6,18 @@ type
   GateFunc* = enum
     gf_AND,
     gf_NAND,
-    gf_OR,
-    gf_NOR,
-    gf_XOR,
-    gf_XNOR,
+    # gf_OR,
+    # gf_NOR,
+    # gf_XOR,
+    # gf_XNOR,
     # gf_A,
     # gf_B,
     # gf_NOT_A,
     # gf_NOT_B
-    gf_NOT_A_AND_B,
-    gf_A_AND_NOT_B,
-    gf_NOT_A_OR_B,
-    gf_A_OR_NOT_B,
+    # gf_NOT_A_AND_B,
+    # gf_A_AND_NOT_B,
+    # gf_NOT_A_OR_B,
+    # gf_A_OR_NOT_B,
     # gf_ONE,
     # gf_ZERO
 
@@ -30,14 +30,14 @@ proc eval*(gf: GateFunc, inputs: seq[seq[int64]]): seq[int64] =
       output.add(bit_and(a, b))
     of gf_NAND:
       output.add(bit_not(bit_and(a, b)))
-    of gf_OR:
-      output.add(bit_or(a, b))
-    of gf_NOR:
-      output.add(bit_not(bit_or(a, b)))
-    of gf_XOR:
-      output.add(bit_xor(a, b))
-    of gf_XNOR:
-      output.add(bit_not(bit_xor(a, b)))
+    # of gf_OR:
+    #   output.add(bit_or(a, b))
+    # of gf_NOR:
+    #   output.add(bit_not(bit_or(a, b)))
+    # of gf_XOR:
+    #   output.add(bit_xor(a, b))
+    # of gf_XNOR:
+    #   output.add(bit_not(bit_xor(a, b)))
     # of gf_A:
     #   output.add(a)
     # of gf_B:
@@ -46,14 +46,14 @@ proc eval*(gf: GateFunc, inputs: seq[seq[int64]]): seq[int64] =
     #   output.add(bit_not(a))
     # of gf_NOT_B:
     #   output.add(bit_not(b))
-    of gf_NOT_A_AND_B:
-      output.add(bit_and(bit_not(a), b))
-    of gf_A_AND_NOT_B:
-      output.add(bit_and(a, bit_not(b)))
-    of gf_NOT_A_OR_B:
-      output.add(bit_or(bit_not(a), b))
-    of gf_A_OR_NOT_B:
-      output.add(bit_or(a, bit_not(b)))
+    # of gf_NOT_A_AND_B:
+    #   output.add(bit_and(bit_not(a), b))
+    # of gf_A_AND_NOT_B:
+    #   output.add(bit_and(a, bit_not(b)))
+    # of gf_NOT_A_OR_B:
+    #   output.add(bit_or(bit_not(a), b))
+    # of gf_A_OR_NOT_B:
+    #   output.add(bit_or(a, bit_not(b)))
     # of gf_ONE:
     #   output.add(bit_not(0'i64)) 
     # of gf_ZERO:
