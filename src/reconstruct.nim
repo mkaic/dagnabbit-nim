3896a18f7bdbd8d6ffb90ecf2fa1786bf1f8115b
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-19 12:49:52 -0700

    trying out profiling

--- src/reconstruct.nim
+++ src/reconstruct.nim
@@ -9,6 +9,7 @@ import std/math
 import std/bitops
 import std/strutils
 import std/random
+import std/nimprof
 
 # TODO: FIX KAHN TOPO SORT AND DESCENDANT-FINDING?
 
@@ -17,11 +18,11 @@ randomize()
 var input_image = pix.read_image("test_images/mona_lisa.jpg")
 
 const
-  width = 8
-  height = 8
+  width = 64
+  height = 96
   channels = 3
   output_bitcount = 8
-  num_gates = 8
+  num_gates = 256
   address_bitcount = fast_log2(width * height * channels) + 1
 
 echo "Total number of addresses: ", width * height * channels
@@ -57,13 +58,8 @@ type MutationType = enum
 var global_best_rmse = 255'f32
 var global_best_image: pix.Image
 var timelapse_count = 0
-var round = 0
-for i in 0..100_000:
-
-  let gate_idx = i mod (graph.gates & graph.outputs).len
-  if gate_idx == 0:
-    round += 1
-  var random_gate = (graph.gates & graph.outputs)[gate_idx]
+for i in 0..1000:
+  var random_gate = sample(graph.gates & graph.outputs)
 
   let mutation_type = rand(MutationType.low..MutationType.high)
   case mutation_type:
@@ -85,15 +81,14 @@ for i in 0..100_000:
 
   let rmse = calculate_rmse(input_image, output_image)
 
-  if i mod 100 == 0:
-    output_image.write_file(&"outputs/timelapse/{timelapse_count:06}.png")
-    output_image.write_file("outputs/latest.png")
-    timelapse_count += 1
-
   if rmse < global_best_rmse:
     global_best_rmse = rmse
     global_best_image = output_image
-    echo &"RMSE: {global_best_rmse:.4f} at step {i:06}. Round: {round}"#. Mutation type: {mutation_type}"
+    echo &"RMSE: {global_best_rmse:.4f} at step {i:06}. Mutation type: {mutation_type}"
+
+    output_image.write_file(&"outputs/timelapse/{timelapse_count:06}.png")
+    output_image.write_file("outputs/latest.png")
+    timelapse_count += 1
 
   elif rmse == global_best_rmse:
     discard # Keep the mutation, but don't count it as an improvement
