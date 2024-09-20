1bd4cfb3a0d3ad333aa4e21063d2d3418fa772eb
Kai Christensen <matthew.kai.christensen@gmail.com>
2024-09-20 16:51:22 -0700

    four AND variants

--- src/gate_funcs.nim
+++ src/gate_funcs.nim
@@ -4,13 +4,13 @@ type
   GateFunc* = enum
     gf_AND,
     gf_NAND,
-    gf_OR,
-    gf_NOR,
-    gf_XOR,
-    gf_XNOR,
-    # gf_A_AND_NOT_B,
+    # gf_OR,
+    # gf_NOR,
+    # gf_XOR,
+    # gf_XNOR,
+    gf_A_AND_NOT_B,
     # gf_A_OR_NOT_B,
-    # gf_NOT_A_AND_B,
+    gf_NOT_A_AND_B,
     # gf_NOT_A_OR_B,
     # gf_A,
     # gf_B,
@@ -26,20 +26,20 @@ proc eval*(gf: GateFunc, a, b: BitArray): BitArray =
       return a and b
     of gf_NAND:
       return not (a and b)
-    of gf_OR:
-      return a or b
-    of gf_NOR:
-      return not (a or b)
-    of gf_XOR:
-      return a xor b
-    of gf_XNOR:
-      return not (a xor b)
-    # of gf_A_AND_NOT_B:
-    #   return a and not b
+    # of gf_OR:
+    #   return a or b
+    # of gf_NOR:
+    #   return not (a or b)
+    # of gf_XOR:
+    #   return a xor b
+    # of gf_XNOR:
+    #   return not (a xor b)
+    of gf_A_AND_NOT_B:
+      return a and not b
     # of gf_A_OR_NOT_B:
     #   return a or not b
-    # of gf_NOT_A_AND_B:
-    #   return not a and b
+    of gf_NOT_A_AND_B:
+      return not a and b
     # of gf_NOT_A_OR_B:
     #   return not a or b
     # of gf_A:
